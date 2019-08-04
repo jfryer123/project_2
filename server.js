2610980b1fceaf10d82394d5a22b318c9ece58a1
Julie Fryer <julie@Julies-MBP-2.fios-router.home>
2019-08-04 19:19:01 -0400

    project commit

--- server.js
+++ server.js
@@ -1,62 +1,98 @@
 // console.ldog(process.env.MONGODB_URI);
-
-//___________________
-//Dependencies
-//___________________
-const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
-const app = express ();
-const db = mongoose.connection;
-//___________________
-//Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3000;
-
-//___________________
-//Database
-//___________________
-// How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI;
-
-// Fix Depreciation Warnings from Mongoose*
-// May or may not need these depending on your Mongoose version
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-
-// Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
-
-// Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
-
-//___________________
-//Middleware
-//___________________
-
-//use public folder for static assets
-app.use(express.static('public'));
-
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+//dependencies
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const session = require('express-session')
+require('dotenv').config()
+const app = express()
 
 
-//___________________
-// Routes
-//___________________
-//localhost:3000
-app.get('/' , (req, res) => {
-  res.send('Hello World!');
+// Configuration
+const PORT = process.env.PORT
+const mongoURI = process.env.MONGO_URI
+
+// Middleware
+// allows us to use put and delete methods
+app.use(methodOverride('_method'))
+// parses info from our input fields into an object
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+// Database
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose.connection.once('open', ()=> {
+  console.log('connected to mongo', mongoURI)
+})
+//add a NEW court page
+app.get('/app/new', (req, res) => {
+  res.render('app/appnew.ejs')
+})
+
+// main login/sign-up
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+  currentUser: req.session.currentUser
+});
+})
+
+//direct to app index page once logged in
+app.get('/app', (req, res)=>{
+  if(req.session.currentUser){
+        res.render('app/appindex.ejs')
+    } else {
+        res.redirect('/sessions/new');
+    }
+})
+
+//edit a court Route
+app.get("/app/:id/edit", (req,res) => {
+    Court.findById(req.params.id, (err, foundCourt) => {
+        res.render("app/appedit.ejs",
+        {
+            court:foundCourt
+        });
+    });
 });
 
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+//app index page
+app.get('/app/appindex', (req, res) => {
+    Court.find({}, (error, allCourts) => {
+        res.render('app/appindex.ejs',{
+            courts:allCourts
+        });
+    })
+});
+
+//show the court page
+app.get('/app/:id', (req, res)=>{
+    Court.findById(req.params.id, (err, foundCourt)=>{
+        res.render('app/appshow.ejs', {
+            court:foundCourt
+        });
+    });
+});
+
+app.post('/app/appindex', (req, res) => {
+    if(req.body.playing === 'on'){
+        req.body.playing = true;
+    } else {
+        req.body.playing = false;
+    }
+    Court.create(req.body, (error, createdCourt) => {
+        res.redirect('/app/appindex');
+    });
+});
+
+const userController = require('./controllers/users.js')
+app.use('/users', userController)
+
+const sessionsController = require('./controllers/sessions.js')
+app.use('/sessions', sessionsController)
+
+// Listen
+app.listen(PORT, () => console.log('auth happening on port', PORT))
